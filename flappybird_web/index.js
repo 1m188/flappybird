b1cadcc674325eb21d6c25450248bc4d910e4a69
1m188 <228117330@qq.com>
2022-06-04 22:51:53 +0800

    图片静态资源

--- flappybird_web/index.js
+++ flappybird_web/index.js
@@ -25,6 +25,97 @@ class Config {
     static background_dx = 0.5;
 };
 
+/**
+ * 图片资源
+ */
+class Res_img {
+
+    /**
+     * 0-9的数字，应是一个数组存储
+     */
+    static numbers = null;
+
+    /**
+     * 白天背景
+     */
+    static background_day = null;
+
+    /**
+     * 黑夜背景
+     */
+    static background_night = null;
+
+    /**
+     * 地面
+     */
+    static base = null;
+
+    /**
+     * 游戏结束标志
+     */
+    static gameover = null;
+
+    /**
+     * 游戏开始信息
+     */
+    static message = null;
+
+    /**
+     * 水管-绿色
+     */
+    static pipe_green = null;
+
+    /**
+     * 水管-红色
+     */
+    static pipe_red = null;
+
+    /**
+     * 蓝鸟-翅膀向下
+     */
+    static bluebird_downflap = null;
+
+    /**
+     * 蓝鸟-翅膀向中
+     */
+    static bluebird_midflap = null;
+
+    /**
+     * 蓝鸟-翅膀向上
+     */
+    static bluebird_upflap = null;
+
+    /**
+     * 红鸟-翅膀向下
+     */
+    static redbird_downflap = null;
+
+    /**
+     * 红鸟-翅膀向中
+     */
+    static redbird_midflap = null;
+
+    /**
+     * 红鸟-翅膀向上
+     */
+    static redbird_upflap = null;
+
+    /**
+     * 黄鸟-翅膀向下
+     */
+    static yellowbird_downflap = null;
+
+    /**
+     * 黄鸟-翅膀向中
+     */
+    static yellowbird_midflap = null;
+
+    /**
+     * 黄鸟-翅膀向上
+     */
+    static yellowbird_upflap = null;
+};
+
 
 let img = new Image();
 img.src = "../asset/image/background-day.png";
