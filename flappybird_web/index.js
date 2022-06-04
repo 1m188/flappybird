0295bb04c81b2499ed8e8eff68bd6f8920efc9be
1m188 <228117330@qq.com>
2022-06-04 22:47:42 +0800

    添加注释

--- flappybird_web/index.js
+++ flappybird_web/index.js
@@ -4,12 +4,24 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+/**
+ * 设置
+ */
 class Config {
 
+    /**
+     * 帧数
+     */
     static FPS = 60;
 
+    /**
+     * 每帧的更新时间
+     */
     static frameUpdateTime = 1000 / this.FPS;
 
+    /**
+     * 背景在每帧中移动的距离
+     */
     static background_dx = 0.5;
 };
 
