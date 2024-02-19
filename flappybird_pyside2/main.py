a7a75923b5bf142d6bd6431caf37af83bd1f29e4
1m188 <228117330@qq.com>
2024-02-19 10:58:57 +0800

    添加高分辨率屏幕自适应支持

--- flappybird_pyside2/main.py
+++ flappybird_pyside2/main.py
@@ -3,6 +3,10 @@ from PySide2.QtWidgets import QApplication
 from window import Window
 import config
 
+from PySide2.QtCore import QCoreApplication, Qt
+
+QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)  # 添加高分屏支持
+
 app = QApplication(sys.argv)
 
 # 加载资源
